9a8cbe0635d1f699b8c02e0ae4c9ae64dee60eaf
Werberth Vinícius <werberthvinicius@gmail.com>
2018-08-05 00:29:39 -0300

    refactor: replace list of books code with ListBooks component

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -5,7 +5,7 @@ class SearchBooks extends Component{
         return (
             <div className="search-books">
         <div className="search-books-bar">
-            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <a className="close-search" onClick={this.props.onChangePage}>Close</a>
             <div className="search-books-input-wrapper">
             <input type="text" placeholder="Search by title or author"/>
             </div>
