cf756a0a28efac55e7883c3138242dcddfdde7c5
Werberth Vinícius <werberthvinicius@gmail.com>
2018-08-05 00:15:25 -0300

    refactor: replace all search book code with SearchBooks component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import * as BooksAPI from './BooksAPI';
+import SearchBooks from './SearchBooks';
+import './App.css';
 
 class BooksApp extends React.Component {
   state = {
@@ -14,29 +15,11 @@ class BooksApp extends React.Component {
   }
 
   render() {
+   
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBooks />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
