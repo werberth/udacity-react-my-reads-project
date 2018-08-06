614448b6025f785d70a9660dfefb72e54fd536cb
Werberth Vinícius <werberthvinicius@gmail.com>
2018-08-06 10:22:39 -0300

    feat: render the books obtained from the API on their respective shelves

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import BookItem from './BookItem';
 
 class BookShelf extends Component {
+
+    getShelfBooks = () => (
+        this.props.books.filter((book) => {
+            console.log(book.shelf, this.props.shelf.id);
+            return book.shelf === this.props.shelf.id;
+        })
+    );
+
     render(){
+        let books = this.getShelfBooks();
+
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <h2 className="bookshelf-title">{this.props.shelf.title}</h2>
                 <div className="bookshelf-books">
-                <ol className="books-grid">
-                    <li>
-                        <BookItem />
-                    </li>
-                </ol>
+                    <ol className="books-grid">
+                        {books.map((book) => (
+                            <li><BookItem book={book}/></li>    
+                        ))}
+                    </ol>
                 </div>
             </div>
         )
