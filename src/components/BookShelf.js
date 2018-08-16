539d5fae2ab826a377b1c46c5e28cd180a2399e9
Werberth Vinícius <werberthvinicius@gmail.com>
2018-08-16 10:40:16 -0300

    feat: define BookShelf componente as stateless component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BookItem from './BookItem';
 import loader from '../icons/loader.gif'
 
-class BookShelf extends Component {
+const BookShelf = (props) => {
+    const {books, shelfTitle, loading, changeBookShelf} = props
+    const empty = !books.length && !loading;
 
-    render(){
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelfTitle}</h2>
+            <div className="bookshelf-books">
+                {loading && <img src={loader} alt="loading..." className="shelf-loader" />}
 
-        const {books, shelfTitle, loading, changeBookShelf} = this.props
-        const empty = !books.length && !loading;
+                {empty && <h3 class="empty-shelf">This shelf is empty</h3>}
 
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelfTitle}</h2>
-                <div className="bookshelf-books">
-                    {loading && <img src={loader} className="shelf-loader" />}
-
-                    {empty && <h3 class="empty-shelf">This shelf is empty</h3>}
-
-                    <ol className="books-grid">
-                        {books.map((book) => (
-                            <li key={book.id}>
-                                <BookItem
-                                    changeBookShelf={changeBookShelf}
-                                    book={book}
-                                />
-                            </li>    
-                        ))}
-                    </ol>
-                </div>
+                <ol className="books-grid">
+                    {books.map((book) => (
+                        <li key={book.id}>
+                            <BookItem
+                                changeBookShelf={changeBookShelf}
+                                book={book}
+                            />
+                        </li>
+                    ))}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BookShelf;
